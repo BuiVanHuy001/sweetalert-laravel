7ebbb46877b942e624a317f45bc3a4f015af9ba7
Bui Van Huy <huy423210063@caodangvietmy.edu.vn>
2025-08-02 18:57:31 +0700

    fix: update SweetAlert2 script source to use minified version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ asset('vendor/sweetalert2/sweetalert2.all.js') }}"></script>
+<script src="{{ asset('vendor/sweetalert2/sweetalert2.all.min.js') }}"></script>
 <script type="module">
     const swalData = @json(session('swal', null));
     if (swalData) Swal.fire(swalData);
